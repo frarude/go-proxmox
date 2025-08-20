5255ca6607315d2ff01fe142004893fe645af1ef
frarude <frank@familie-rust.de>
2025-08-20 13:39:45 +0200

    Update magefile.go

--- magefile.go
+++ magefile.go
@@ -12,10 +12,10 @@ import (
 	"github.com/magefile/mage/sh"
 
 	//mage:import install
-	"github.com/luthermonson/go-proxmox/mage/install"
+	"github.com/frarude/go-proxmox/mage/install"
 
 	//mage:import test
-	"github.com/luthermonson/go-proxmox/mage/test"
+	"github.com/frarude/go-proxmox/mage/test"
 )
 
 var (
